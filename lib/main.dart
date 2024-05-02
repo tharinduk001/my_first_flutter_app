8effd510be62920abcdf6917d419cc5bd5a678db
Tharindu Kalhara <136310961+tharinduk001@users.noreply.github.com>
2024-05-02 09:29:41 +0530

    project finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -10,13 +10,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
         home: Scaffold(
       appBar: AppBar(
-        title: Text("Easy Flutter App"),
-        backgroundColor: Colors.blue,
-        leading: Icon(Icons.menu),
-        actions: [Icon(Icons.search)],
+        title: const Text(
+          "Easy Flutter App",
+          style: TextStyle(color: Colors.white, fontSize: 25, fontWeight: FontWeight.w400),
+        ),
+        backgroundColor:const  Color.fromARGB(255, 96, 74, 224),
+        leading: const Icon(Icons.menu, size: 30, color: Colors.white),
+        actions: const [
+          Icon(
+            Icons.search,
+            size: 30,
+            color: Colors.white,
+          )
+        ],
       ),
+      body:const  Center(child: Text("Hello I'm Flutter.",style: TextStyle(fontSize: 25),)),
     ));
   }
 }
